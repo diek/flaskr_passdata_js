992bbeb2002dc3cfd254ce3b1ec651a07aab452f
diek <diek@bellaliant.net>
2015-07-20 03:27:00 -0300

    more mods

--- static/pass_data.js
+++ static/pass_data.js
@@ -1,11 +1,11 @@
 // a function to take in the list of data, created in app.py
 // add option items to the dom using the values.
-function displayData(in_data) {
+function displayData(wings) {
   var selectWings = document.getElementById('chickenWings')
   var fragment = document.createDocumentFragment()
-  for (i = 0; i < in_data.length; i++){
+  for (i = 0; i < wings.length; i++){
     var option = document.createElement("option");
-    option.text = in_data[i];
+    option.text = wings[i];
     fragment.appendChild(option);
   }
   selectWings.appendChild(fragment);
