4838fb65eab84ac80a8b2de0be7071b09821a0e5
diek <diek@bellaliant.net>
2015-07-20 03:24:53 -0300

    more mods

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
-from flask import Flask, render_template  # , send_from_directory
-# import os
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
 
 
-# @app.route('/pass_data.js')
-# def js():
-#     stuff = "greasy"
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                                'pass_data.js', stuff=stuff)
-
-
 @app.route("/")
 def get_data():
-    # a sample data set to for options to an html select.
-    my_data = ["Uncle Lou's Sweet Spicy Love", "Diek's Wings of the Chicken", "Asian Flame Throwers", "Twisted BBQ", "Dusted and Crispy"]
-    return render_template('data.html', my_data=json.dumps(my_data))
+    # a sample data set, for options for an html select.
+    wing_options = ["Uncle Lou's Sweet Spicy Love", "Diek's Wings of the Chicken", "Asian Flame Throwers", "Twisted BBQ", "Dusted and Crispy"]
+    return render_template('data.html', wing_options=json.dumps(wing_options))
 
 
 if __name__ == '__main__':
